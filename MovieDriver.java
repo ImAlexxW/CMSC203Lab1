048a4f2079a31fb0541196da58ec51f417ae6cca
Alexander Wissdorf <102696715+ImAlexxW@users.noreply.github.com>
2022-09-13 15:46:57 -0400

    Add files via upload

---
+++ MovieDriver.java
@@ -0,0 +1,14 @@
+
+public class MovieDriver 
+{
+
+	public static void main(String[] args) 
+	//Create a movie object named m1
+	{
+		Movie m1 = new Movie();
+		m1.setTitle("Spiderman");
+		System.out.println(m1.getTitle());
+		
+	}
+
+}
